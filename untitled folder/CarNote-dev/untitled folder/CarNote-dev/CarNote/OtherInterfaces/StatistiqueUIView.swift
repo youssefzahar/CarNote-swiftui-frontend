093842c89448fe84ccd56ff039b1d0d1663ae94b
DhaouadiMariem <74209337+DhaouadiMariem@users.noreply.github.com>
2022-12-17 20:51:25 +0100

    NewUI

---
+++ untitled folder/CarNote-dev/untitled folder/CarNote-dev/CarNote/OtherInterfaces/StatistiqueUIView.swift
@@ -0,0 +1,20 @@
+//
+//  StatistiqueUIView.swift
+//  carNote
+//
+//  Created by youssef-mariem on 21/11/2022.
+//
+
+import SwiftUI
+
+struct StatistiqueUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatistiqueUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatistiqueUIView()
+    }
+}
